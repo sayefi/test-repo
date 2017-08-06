4339db9e4341efd41d11d8a59e8fc61b537ee9bf
sayef <sayefi@gmail.com>
2017-08-06 12:55:54 -0400

    test file added

---
+++ test.r
@@ -0,0 +1,15 @@
+x <-5
+y<-x+2
+x
+y
+
+
+
+# this is my first R script
+hello <- "hello world"
+
+??read
+
+testData<-c(1:10)
+
+write.csv(testData,file="testoutput.csv",sep=",", eol="\n",row.names=FALSE)
