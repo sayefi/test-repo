c3ee0a762defbabd4a95b275263cfd224a617889
sayef <sayefi@gmail.com>
2017-08-13 01:29:33 -0400

    added missed file usemyfunction.r

---
+++ usemyfunction.r
@@ -0,0 +1,16 @@
+## Sample code to load and use myfunction
+
+
+## see working directory
+getwd()
+
+
+# Set the working directory
+setwd("C:/Users/sayef/Documents/Data_Science/Projects/test-repo")
+
+
+### Load myfunction
+
+source("mycode.r")
+ls()
+
