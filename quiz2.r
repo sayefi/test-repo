d90669ad9f6426adf621a203e1c6c5cfba369689
sayef <sayefi@gmail.com>
2017-08-14 22:03:08 -0400

    resubmitted quiz1

--- quiz2.r
+++ quiz2.r
@@ -1,8 +1,68 @@
 
 
 
+
+x<-4
+class(x)
+
+x<-c(4,TRUE)
+class(x)
+
+x<-list(2,"a","b",TRUE)
+y<-x[[2]]
+length(y)
+y
+
+x<-c(1:4)
+x
+y<-2
+
+x+y
+
 # Set the working directory
 setwd("C:/Users/sayef/Documents/Data_Science/Projects/test-repo")
+
+## Reading from file
+con<-file("hw1_data.csv","r")
+quiz1_data<-read.csv(con)
+close(con)
+quiz1_data
+
+## print column names of the dataset
+names(quiz1_data)
+
+## print first 2 rows
+head(quiz1_data,2)
+
+## number of observations
+nrow(quiz1_data)
+
+## print last 2 rows
+tail(quiz1_data,2)
+
+## value of Ozone in 47th row
+quiz1_data[47,"Ozone"]
+
+## number of missing values in ozone column
+nrow(quiz1_data[is.na(quiz1_data$Ozone),])
+
+## mean of the Ozone column in this dataset
+mean(quiz1_data[!is.na(quiz1_data$Ozone),]$Ozone)
+
+
+## Ozone values are above 31 and Temp values are above 90. What is the mean of Solar.R in this subset?
+solar<-quiz1_data[quiz1_data$Ozone>31 & quiz1_data$Temp>90,]
+mean(solar[!is.na(solar$Solar.R),]$Solar.R)
+
+## What is the mean of "Temp" when "Month" is equal to 6?
+temp<-quiz1_data[quiz1_data$Month==6,]
+mean(temp$Temp)
+mean(temp[!is.na(temp$Temp),]$Temp)
+
+## What was the maximum ozone value in the month of May 
+temp<-quiz1_data[quiz1_data$Month==5,]
+mean(temp$Temp)
+mean(temp[!is.na(temp$Temp),]$Temp)
 
 read.csv("hw1_data.csv",nrows=2)
 
